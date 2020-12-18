742467c3eb066131d8f03fb697dcfe7ad0db108d
tangb <tangb@kfconcept.com>
2020-12-18 16:46:00 +0800

    feat: add a non-owning byte input view

---
+++ include/StreamPacketParser/ByteView.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <stdexcept>
+#include <vector>
+
+namespace spp {
+using Bytes = std::vector<std::uint8_t>;
+
+// Non-owning input view. The caller keeps the underlying bytes alive.
+class ByteView {
+public:
+    ByteView() noexcept = default;
+    ByteView(const std::uint8_t* data, std::size_t size) : data_(data), size_(size) {
+        if (!data && size != 0) throw std::invalid_argument("null data with nonzero size");
+    }
+    explicit ByteView(const Bytes& bytes) : ByteView(bytes.data(), bytes.size()) {}
+    const std::uint8_t* data() const noexcept { return data_; }
+    std::size_t size() const noexcept { return size_; }
+    bool empty() const noexcept { return size_ == 0; }
+    const std::uint8_t* begin() const noexcept { return data_; }
+    const std::uint8_t* end() const noexcept { return size_ ? data_ + size_ : data_; }
+    std::uint8_t operator[](std::size_t index) const noexcept { return data_[index]; }
+    ByteView subview(std::size_t offset, std::size_t count) const {
+        if (offset > size_ || count > size_ - offset)
+            throw std::out_of_range("byte view range");
+        return ByteView(offset ? data_ + offset : data_, count);
+    }
+private:
+    const std::uint8_t* data_ = nullptr;
+    std::size_t size_ = 0;
+};
+} // namespace spp
