c366322c2a3153a3fc364f5c5f2422e017eba5be
tangb <tangb@kfconcept.com>
2024-12-13 11:07:00 +0800

    test: cover overlapping header and length fields

--- tests/StreamPacketParser.Tests/PublicApiSkeletonTests.cs
+++ tests/StreamPacketParser.Tests/PublicApiSkeletonTests.cs
@@ -86,4 +86,19 @@ public sealed class PublicApiSkeletonTests
 
         Assert.Same(validator, options.Validator);
     }
+
+    [Fact]
+    public void CreateLengthFieldProtocol_HeaderOverlapsLengthField_Throws()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => PacketParserOptions.CreateLengthFieldProtocol(
+            header: new byte[] { 0xAA, 0x55 },
+            lengthFieldOffset: 1,
+            lengthFieldSize: 2,
+            byteOrder: ByteOrder.BigEndian,
+            lengthMode: FrameLengthMode.PayloadLength,
+            fixedFrameOverhead: 8,
+            minFrameLength: 8,
+            maxFrameLength: 64,
+            maxBufferedBytes: 128));
+    }
 }
